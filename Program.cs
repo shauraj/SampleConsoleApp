f4acd87ecb8e80b31b87c441392b533e25100bd6
shauraj <49944907+shauraj@users.noreply.github.com>
2025-06-19 13:26:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");Console.WriteLine("Hello, World!");Console.WriteLine("Hello, World!");Console.WriteLine("Hello, World!");Console.WriteLine("Hello, World!");
+Console.ReadLine();
+// This is a simple C# console application that prints "Hello, World!" to the console.
+// It also waits for user input before closing, allowing you to see the output.
+// To run this code, you can use the .NET SDK. Make sure you have it installed. 
+// You can create a new console application using the command:
+// dotnet new console -n HelloWorldApp  
+Console.ReadKey();  
+// After creating the project, replace the content of Program.cs with the code above.
+// Then, you can run the application using the command:
+// dotnet run
+// This will compile and execute the code, displaying "Hello, World!" in the console.
+// You can also modify the message or add more functionality as needed.
